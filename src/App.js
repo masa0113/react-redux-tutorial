e29f4b46fa7baeba524dedca08c9eacffea9a83b
masahiro.fukuyama <be.fukuyama.m@gmail.com>
2021-01-10 15:25:46 +0900

    commit jsx tutorial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render(){
-    return <div><h1>Hello, world!</h1></div>;
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => console.log("clicked")} />
+      </React.Fragment>
+    );
   }
 }
 
